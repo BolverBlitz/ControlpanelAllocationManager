e5ed03570c4abf56d0767edf3e8bf84f57e275b7
Marc <bolverblitz@ebg.pw>
2022-02-21 04:11:03 +0100

    Version 1,0
    Bugfixes & added logic

--- src/index.js
+++ src/index.js
@@ -1,54 +1,88 @@
 const db = require('../lib/db/mysql');
 const storage = require('../storage/index');
+const { logger } = require('../lib/logger');
 let product_cache = {}; //Stores product IDs as key with the human readable name as value
 const init = []
 
-init.push(db.controlpanel.get.GetProductNameAndID())
+if (process.env.NodeLoadPercent > 100) { throw new Error('NodeLoadPercent must be less or equel to 100%'); }
+
+init.push(db.controlpanel.get.ProductNameAndID())
 init.push(storage.CheckIfFileExists('node_producs'))
 
 Promise.all(init).then(function (values) {
-    const [GetProductNameAndID, node_producs] = values; 
+    const [GetProductNameAndID, node_producs] = values;
 
     GetProductNameAndID.forEach(function (row) {
         product_cache[row.id] = row.name;
     });
 
     if (!node_producs) {
-        console.log("Running first time, creating node_producs file");
-        db.controlpanel.get.GetNodes().then(function (nodes) {
+        logger('system', 'Running first time, creating node_producs file');
+        db.controlpanel.get.Nodes().then(function (nodes) {
             let node_producs = {};
             nodes.forEach(function (node) {
                 node_producs[node.id] = [];
             });
 
-            db.controlpanel.get.GetNodesProductLinks().then(function (links) {
+            db.controlpanel.get.NodesProductLinks().then(function (links) {
                 links.forEach(function (link) {
                     node_producs[link.node_id].push(link.product_id);
                 });
                 storage.WriteFile('node_producs', node_producs).catch(function (err) {
-                    console.log(err);
+                    logger('error', err);
                 });
             });
         });
     }
 
     checknodes();
-    //console.log(product_cache, node_producs);
+
+    setInterval(() => {
+        checknodes();
+    }, process.env.CheckDelayInMS);
 }).catch(function (err) {
     console.log(err);
 });
 
 const checknodes = function () {
-    db.controlpanel.get.GetNodes().then(function (nodes) {
-        nodes.forEach(function (node) {
-            Promise.all([db.pterodactyl.get.GetNodeResources(node.id), db.pterodactyl.get.GetNodeUsage(node.id)]).then(function (values) {
-                const [resources, usage] = values; 
-                console.log(resources, usage);
-                /*
-                if (usage[0].sum_memory > 80) {
-                    console.log(`Node ${node.name} is over 80% CPU usage`);
-                }
-                */
+    db.controlpanel.get.Nodes().then(function (nodes) {
+        Promise.all([storage.ReadFile('node_producs'), db.controlpanel.get.NodesProductLinks()]).then(function (values) {
+            const [file_node_producs, node_producs] = values;
+
+            let currentavticenodelist = [];
+            node_producs.forEach(function (link) {
+                currentavticenodelist.push(link.node_id);
+            });
+
+            let jobs = [];
+
+            nodes.forEach(function (node) {
+                Promise.all([db.pterodactyl.get.NodeResources(node.id), db.pterodactyl.get.NodeUsage(node.id)]).then(function (values) {
+                    const [resources, usage] = values;
+
+                    const used_memory = (usage.sum_memory / (resources.memory + (resources.memory * (resources.memory_overallocate / 100))) * 100).toFixed(0);
+                    const used_disk = (usage.sum_disk / (resources.disk + (resources.disk * (resources.disk_overallocate / 100))) * 100).toFixed(0);
+
+                    
+
+                    if(Number(used_memory) > Number(process.env.NodeLoadPercent) || Number(used_disk) > Number(process.env.NodeLoadPercent)) {
+                        logger('info', `Node ${node.name} is over ${process.env.NodeLoadPercent}% memory usage. Current usage ${used_memory}%`);
+                        if(currentavticenodelist.indexOf(node.id) !== -1) {
+                            file_node_producs[node.id].forEach(function (product_id) {
+                                logger('info', `Should deleting ${product_cache[product_id]} from ${node.name}`);
+                                jobs.push(db.controlpanel.del.ProductNodeLink(node.id, product_id));
+                            });
+                        }
+                    }else{
+                        logger('info', `Node ${node.name} is under ${process.env.NodeLoadPercent}% memory usage. Current usage ${used_memory}%`);
+                        if(currentavticenodelist.indexOf(node.id) === -1) {
+                            file_node_producs[node.id].forEach(function (product_id) {
+                                logger('info', `Should adding ${product_cache[product_id]} to ${node.name}`);
+                                jobs.push(db.controlpanel.add.ProductNodeLink(node.id, product_id));
+                            });
+                        }
+                    }
+                });
             });
         });
     });
