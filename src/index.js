52451cf0bae2f7f8b8cfa761dad869f2a7e116ea
BolverBlitz <bolverblitz@ebg.pw>
2022-07-19 21:07:41 +0200

    Added CPU monitoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const db = require('../lib/db/mysql');
 const storage = require('../storage/index');
 const { logger } = require('../lib/logger');
+const { GetPteroStatusData } = require('../lib/pterostatus/index');
 let product_cache = {}; //Stores product IDs as key with the human readable name as value
 const init = []
 
@@ -46,8 +47,13 @@ Promise.all(init).then(function (values) {
 
 const checknodes = function () {
     db.controlpanel.get.Nodes().then(function (nodes) {
-        Promise.all([storage.ReadFile('node_producs'), db.controlpanel.get.NodesProductLinks()]).then(function (values) {
-            const [file_node_producs, node_producs] = values;
+        const Base_promises = [storage.ReadFile('node_producs'), db.controlpanel.get.NodesProductLinks()];
+
+        if (process.env.pterostatus_enable === 'true' || process.env.pterostatus_enable === true) {
+            Base_promises.push(GetPteroStatusData());
+        }
+        Promise.all(Base_promises).then(function (values) {
+            const [file_node_producs, node_producs, pterostatus_usage] = values;
 
             let currentavticenodelist = [];
             node_producs.forEach(function (link) {
@@ -63,21 +69,47 @@ const checknodes = function () {
                     const used_memory = (usage.sum_memory / (resources.memory + (resources.memory * (resources.memory_overallocate / 100))) * 100).toFixed(0);
                     const used_disk = (usage.sum_disk / (resources.disk + (resources.disk * (resources.disk_overallocate / 100))) * 100).toFixed(0);
 
-                    if(Number(used_memory) > Number(process.env.NodeLoadPercent) || Number(used_disk) > Number(process.env.NodeLoadPercent)) {
-                        logger('info', `Node ${node.name} is over ${process.env.NodeLoadPercent}% memory usage. Current usage ${used_memory}%`);
-                        if(currentavticenodelist.indexOf(node.id) !== -1) {
-                            file_node_producs[node.id].forEach(function (product_id) {
-                                logger('warning', `Should deleting ${product_cache[product_id]} from ${node.name}`);
-                                jobs.push(db.controlpanel.del.ProductNodeLink(node.id, product_id));
-                            });
-                        }
-                    }else{
-                        logger('info', `Node ${node.name} is under ${process.env.NodeLoadPercent}% memory usage. Current usage ${used_memory}%`);
-                        if(currentavticenodelist.indexOf(node.id) === -1) {
-                            file_node_producs[node.id].forEach(function (product_id) {
-                                logger('warning', `Should adding ${product_cache[product_id]} to ${node.name}`);
-                                jobs.push(db.controlpanel.add.ProductNodeLink(node.id, product_id));
-                            });
+                    //Check CPU load (Only if pterostatus is enabled)
+                    if (pterostatus_usage) {
+                        pterostatus_usage.map(function (usage) {
+                            if (usage.nodeName.trim().replace(" ", "") === node.name.trim().replace(" ", "")) {
+                                if (pterostatus_usage[node.id - 1].PS_CPULoad > Number(process.env.NodeLoadPercent) || Number(used_memory) > Number(process.env.NodeLoadPercent) || Number(used_disk) > Number(process.env.NodeLoadPercent)) {
+                                    logger('info', `Node ${node.name} is over ${process.env.NodeLoadPercent}% CPU/Disk/Memory usage. Current usage CPU:${pterostatus_usage[node.id - 1].PS_CPULoad}%, Memory:${used_memory}%, Disk:${used_disk}%`);
+                                    if (currentavticenodelist.indexOf(node.id) !== -1) {
+                                        file_node_producs[node.id].forEach(function (product_id) {
+                                            logger('warning', `Should deleting ${product_cache[product_id]} from ${node.name}`);
+                                            jobs.push(db.controlpanel.del.ProductNodeLink(node.id, product_id));
+                                        });
+                                    }
+                                } else {
+                                    logger('info', `Node ${node.name} is under ${process.env.NodeLoadPercent}% CPU/Disk/Memory usage. Current usage CPU:${pterostatus_usage[node.id - 1].PS_CPULoad}%, Memory:${used_memory}%, Disk:${used_disk}%`);
+                                    if (currentavticenodelist.indexOf(node.id) === -1) {
+                                        file_node_producs[node.id].forEach(function (product_id) {
+                                            logger('warning', `Should adding ${product_cache[product_id]} to ${node.name}`);
+                                            jobs.push(db.controlpanel.add.ProductNodeLink(node.id, product_id));
+                                        });
+                                    }
+                                }
+                            }
+                        });
+                    } else {
+                        //Check memory and disk allocation
+                        if (Number(used_memory) > Number(process.env.NodeLoadPercent) || Number(used_disk) > Number(process.env.NodeLoadPercent)) {
+                            logger('info', `Node ${node.name} is over ${process.env.NodeLoadPercent}% memory or disk usage. Current usage MEM:${used_memory}% & DISK:${used_disk}%`);
+                            if (currentavticenodelist.indexOf(node.id) !== -1) {
+                                file_node_producs[node.id].forEach(function (product_id) {
+                                    logger('warning', `Should deleting ${product_cache[product_id]} from ${node.name}`);
+                                    jobs.push(db.controlpanel.del.ProductNodeLink(node.id, product_id));
+                                });
+                            }
+                        } else {
+                            logger('info', `Node ${node.name} is under ${process.env.NodeLoadPercent}% memory or disk usage. Current usage MEM:${used_memory}% & DISK:${used_disk}%`);
+                            if (currentavticenodelist.indexOf(node.id) === -1) {
+                                file_node_producs[node.id].forEach(function (product_id) {
+                                    logger('warning', `Should adding ${product_cache[product_id]} to ${node.name}`);
+                                    jobs.push(db.controlpanel.add.ProductNodeLink(node.id, product_id));
+                                });
+                            }
                         }
                     }
                 }).catch(function (err) {
